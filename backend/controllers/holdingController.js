ff9401de0fac09c9d55602ea89d9beaf1b37611a
Saranyasarathy V <saranyasarathy7@gmail.com>
2025-08-04 13:47:33 +0530

    Added the fetch holdings and fetch holding by ID functionality

--- backend/controllers/holdingController.js
+++ backend/controllers/holdingController.js
@@ -34,6 +34,60 @@ const createHolding = async (req, res) => {
 
 }
 
+// Function to get all the holdings
+const getAllHoldings = async (req, res) => {
+
+    try {
+
+        const holdings = await Holding.findAll()
+        if (!holdings) {
+            return res.status (404).json({
+                message: 'No Holding Found!'
+            })
+        }
+
+        res.status(200).json({
+            message: 'Sure! Here are a list of all the holdings created!',
+            holdings
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: `Error fetching the holding: ${err}`
+        })
+    }
+
+}
+
+// Function to fetch a holding by a specific ID (hid)
+const getHoldingById = async (req, res) => {
+
+    try {
+
+        const { id } = req.params 
+
+        const holdingMatch = await Holding.findByPk(id)
+        if (!holdingMatch) {
+            return res.status (404).json({
+                message: 'No Holding Found!'
+            })
+        }
+
+        res.status(200).json({
+            message: 'Sure! Your requested ID returned a match.',
+            holdingMatch
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: `Error creating the holding: ${err}`
+        })
+    }
+
+}
+
 module.exports = {
-    createHolding
+    createHolding,
+    getAllHoldings,
+    getHoldingById
 }
--- backend/routes/holdingRoutes.js
+++ backend/routes/holdingRoutes.js
@@ -1,7 +1,19 @@
-const { createHolding } = require('../controllers/holdingController')
+const { createHolding, getAllHoldings, getHoldingById } = require('../controllers/holdingController')
 const express = require('express')
 
 const router = express.Router()
+
+/*
+    GET Request -> Get all the holdings
+    /api/holding
+*/
+router.get('/', getAllHoldings)
+
+/*
+    GET Request -> Get the holding of a specific ID
+    /api/holding/:id
+*/
+router.get('/:id', getHoldingById)
 
 /*
     POST Request -> Create a new holding
